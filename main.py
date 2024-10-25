b34823864bebcfffe6263dd843bac91913dd29dc
Bletsch22 <kylebletscher49@gmail.com>
2024-10-24 20:38:06 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import random
+
+num = random.randint(1,6)
+
+
+if num == int("1"):
+    print("     ")
+    print("  X  ")
+    print("     ")
+
+elif num == int("2"):
+    print("  X  ")
+    print("     ")
+    print("  X  ")
+        
+elif num == int("3"):
+    print("X    ")
+    print("  X  ")
+    print("    X")
+        
+elif num == int("4"):
+    print("X   X")
+    print("X   X")
+    print("     ")
+        
+elif num == int("5"):
+    print("X   X")
+    print("  X  ")
+    print("X   X")
+
+elif num == int("6"):
+    print("X   X")
+    print("X   X")
+    print("X   X")
+    
+    
+
+    
+    
